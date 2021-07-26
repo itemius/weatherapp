4e60874413d80d471947d41d73ee7035b8ee07db
itemius <itemius@gmail.com>
2021-07-26 10:35:00 +0300

    Update Weather.swift
    Unused code removed

--- weatherapp/Model/Weather.swift
+++ weatherapp/Model/Weather.swift
@@ -24,13 +24,7 @@ extension Weather: Decodable {
     enum CodingKeys: String, CodingKey {
         case weather
         case main
-        
-//        enum WeatherKeys: String, CodingKey {
-//            case main
-//            case icon
-//            case description
-//        }
-        
+                
         enum MainKeys: String, CodingKey {
             case temperature = "temp"
         }
@@ -39,17 +33,12 @@ extension Weather: Decodable {
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         
-//        let weatherContainer = try container.nestedContainer(keyedBy: CodingKeys.WeatherKeys.self, forKey: .weather)
         let weatherArray = try container.decode([WeatherBase].self, forKey: .weather)
         
         main = weatherArray.first!.main
         icon = weatherArray.first!.icon
         description = weatherArray.first!.description
 
-//        main = try weatherContainer.decode(String.self, forKey: .main)
-//        icon = try weatherContainer.decode(String.self, forKey: .icon)
-//        description = try weatherContainer.decode(String.self, forKey: .description)
-//
         let mainContainer = try container.nestedContainer(keyedBy: CodingKeys.MainKeys.self, forKey: .main)
         temperature = try mainContainer.decode(Double.self, forKey: .temperature)
     }
